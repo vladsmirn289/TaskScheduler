5b4dca4f742df254635cb98962bf0d04e867f424
Vladislav <vladsmirn289@gmail.com>
2020-11-08 14:32:01 +1100

    Init project

---
+++ src/test/java/com/scheduler/TaskScheduler/TaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduler.TaskScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
